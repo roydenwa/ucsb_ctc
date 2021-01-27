70386fbaa3ec8142ba86fae06dbe623aae33745e
roydenwa <royden-wagner@outlook.com>
2021-01-27 20:19:02 +0100

    refactor: parallelized track_main_fast()

--- track_fast.py
+++ track_fast.py
@@ -3,13 +3,28 @@ import numba
 import numpy as np
 import pandas as pd
 import networkx as nx
+import time as timing
+import SimpleITK as sitk
+import concurrent.futures
 
 from networkx.algorithms import similarity
+
+
+def save_img_as_tiff(seg_img: np.ndarray, time: int, track_save_dir: str):
+    """
+    helper-func to parallelize saving imgs as tiff-files 
+    """
+    img = sitk.GetImageFromArray(seg_img.astype("uint16"))
+    filename = 'mask'+'%0*d' % (3, time) + '.tif'
+    sitk.WriteImage(img, os.path.join(track_save_dir, filename))
 
 
 @numba.njit(parallel=True)
 def compute_cell_center(seg_img: np.ndarray, labels: np.ndarray, results: np.ndarray) \
                         -> np.ndarray:
+    """
+    jitted helper-func to compute cell-centers
+    """
     for label in labels:
         if label != 0:
             all_points_z, all_points_x, all_points_y = np.where(seg_img == label)
@@ -65,18 +80,21 @@ def compute_cell_location_fast(seg_img: np.ndarray, all_labels: np.ndarray) \
     return g, centers
 
 
-def tracklet_fast(g1, g2, seg_img1, seg_img2, maxtrackid, time, linelist, tracksavedir, cellcenter1, cellcenter2):
+def tracklet_fast(g1: nx.graph, g2: nx.graph, seg_img1: np.ndarray, seg_img2: np.ndarray, maxtrackid: int, time: int, 
+                  linelist: list, tracksavedir: str, cellcenter1: np.ndarray, cellcenter2: np.ndarray) \
+                  -> (int, list):
     """
     optimized version of tracklet()
 
-    speed gained by reusing previously calulated cellcenters
+    speed gained by reusing previously calulated cellcenters and
+    parallelized IO
     """
     f1 = {}
     f2 = {}
-    new_seg_img2 = np.zeros(seg_img2.shape)
     dict_associate = {}
-    loc1 = g1.degree(weight='weight')
-    loc2 = g2.degree(weight='weight')
+    loc1 = g1.degree(weight="weight")
+    loc2 = g2.degree(weight="weight")
+    new_seg_img2 = np.zeros(seg_img2.shape)
 
     for ele1 in loc1:
         cell = ele1[0]
@@ -86,7 +104,6 @@ def tracklet_fast(g1, g2, seg_img1, seg_img2, maxtrackid, time, linelist, tracks
         cell = ele2[0]
         f2[cell] = [cellcenter2[cell], ele2[1]]
 
-    # TODO: try to write external and jitted version for those loops
     for cell in f2.keys():
         tmp_center = f2[cell][0]
         min_distance = seg_img2.shape[0]**2 + seg_img2.shape[1]**2 + seg_img2.shape[2]**2
@@ -109,7 +126,6 @@ def tracklet_fast(g1, g2, seg_img1, seg_img2, maxtrackid, time, linelist, tracks
 
     maxtrackid = max(maxtrackid, max(inverse_dict_ass.keys()))
 
-    # TODO: try to write external and jitted version for those loops
     for cell in inverse_dict_ass.keys():
         if len(inverse_dict_ass[cell]) > 1:
             for cellin2 in inverse_dict_ass[cell]:
@@ -129,17 +145,57 @@ def tracklet_fast(g1, g2, seg_img1, seg_img2, maxtrackid, time, linelist, tracks
                     new_string = '{} {} {} {}'.format(list_tmp[0], list_tmp[1], time+1, list_tmp[3])
                     linelist[i-1] = new_string
 
-    # TODO: each get and write gets one thread to do it, so it's parallel not sequential
-    img1 = sitk.GetImageFromArray(seg_img1.astype('uint16'))
-    img2 = sitk.GetImageFromArray(new_seg_img2.astype('uint16'))
-
-    filename1 = 'mask'+'%0*d' % (3, time)+'.tif'
-    filename2 = 'mask'+'%0*d' % (3, time+1)+'.tif'
-
-    sitk.WriteImage(img1, os.path.join(tracksavedir, filename1))
-    sitk.WriteImage(img2, os.path.join(tracksavedir, filename2))
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        thread1 = executor.submit(save_img_as_tiff, seg_img1, time, tracksavedir)
+        thread2 = executor.submit(save_img_as_tiff, new_seg_img2, time+1, tracksavedir)
 
     return maxtrackid, linelist
+
+
+def compute_graph_and_cellcenters_img1(img1: np.ndarray, time: int, linelist: list, maxtrackid: int) \
+                                       -> (nx.graph, np.ndarray, list, int):
+    """
+    helper-func to parallelize the computation of the graph and the cellcenters
+    for img1
+    """
+    labels = compute_unique_vals(img1, return_counts=False)
+    g1, cellcenter1 = compute_cell_location_fast(img1, labels)
+
+    if time == 0:
+        for cell in labels:
+            if cell != 0:
+                string = '{} {} {} {}'.format(cell, time, time, 0)
+                linelist.append(string)
+            maxtrackid = max(cell, maxtrackid)
+    
+    return g1, cellcenter1, linelist, maxtrackid
+
+
+def compute_graph_and_cellcenters_img2(img1: np.ndarray, folder2: str, time: int, threshold: int) \
+                                       -> (np.ndarray, nx.graph, np.ndarray):
+    """
+    helper-func to parallelize the computation of the graph and the cellcenters
+    for img2
+    """
+    file2 = 'mask'+'%0*d' % (3, time+1) +'.tif'
+    img2_sitk = sitk.ReadImage(os.path.join(folder2, file2))
+    img2 = sitk.GetArrayFromImage(img2_sitk)
+
+    img2_label_counts = np.array(compute_unique_vals(img2, return_counts=True)).T
+    labels2 = img2_label_counts[..., 0]
+
+    if len(labels2) < 2:
+        img2 = img1
+        img2_img = sitk.GetImageFromArray(img2)
+        sitk.WriteImage(img2_img, os.path.join(folder2, file2))
+
+    for i, label in enumerate(labels2):
+        if img2_label_counts[i, 1] < threshold:
+            img2[img2 == label] = 0
+
+    g2, cellcenter2 = compute_cell_location_fast(img2, labels2)
+
+    return img2, g2, cellcenter2
 
 
 def track_main_fast(seg_fold: str, track_fold: str):
@@ -147,14 +203,14 @@ def track_main_fast(seg_fold: str, track_fold: str):
     optimized version of track_main()
 
     speed gained by reducing redundancy,
-    using accelerated helper-funcs and
-    parallelized IO -> TODO
+    using accelerated and parallel helper-funcs and
+    parallelizing IO in general
     """
+    linelist = []
+    maxtrackid = 0
     folder1 = track_fold
     folder2 = seg_fold
     times = len(os.listdir(folder2))
-    maxtrackid = 0
-    linelist = []
     total_start_time = timing.time()
 
     for time in range(times-1):
@@ -166,16 +222,15 @@ def track_main_fast(seg_fold: str, track_fold: str):
             file1 = 'mask000.tif'
             img1 = sitk.ReadImage(os.path.join(folder2, file1))
             img1 = sitk.GetArrayFromImage(img1)
-            img1_label, img1_counts = np.unique(img1, return_counts=True)
+            img1_label, img1_counts = compute_unique_vals(img1, return_counts=True)
 
             for l in range(len(img1_label)):
                 if img1_counts[l] < threshold:
                     img1[img1 == img1_label[l]] = 0
 
-            labels = pd.unique(img1.flatten('K'))
+            labels = compute_unique_vals(img1, return_counts=False)
             start_label = 0
 
-            # seems odd...
             for label in labels:
                 img1[img1 == label] = start_label
                 start_label = start_label + 1
@@ -184,37 +239,17 @@ def track_main_fast(seg_fold: str, track_fold: str):
             sitk.WriteImage(img1, os.path.join(folder1, file1))
 
         file1 = 'mask'+'%0*d' % (3, time)+'.tif'
-        file2 = 'mask'+'%0*d' % (3, time+1)+'.tif'
-        img1 = sitk.ReadImage(os.path.join(folder1, file1))
-        img2 = sitk.ReadImage(os.path.join(folder2, file2))
-        img1 = sitk.GetArrayFromImage(img1)
-        img2 = sitk.GetArrayFromImage(img2)
+        img1_sitk = sitk.ReadImage(os.path.join(folder1, file1))
+        img1 = sitk.GetArrayFromImage(img1_sitk)
 
-        img2_label_counts = np.array(np.unique(img2, return_counts=True)).T
-        labels2 = img2_label_counts[..., 0]
+        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+            thread1 = executor.submit(compute_graph_and_cellcenters_img1, img1, time, linelist, maxtrackid)
+            thread2 = executor.submit(compute_graph_and_cellcenters_img2, img1, folder2, time, threshold)
+            
+            g1, cellcenter1, linelist, maxtrackid = thread1.result()
+            img2, g2, cellcenter2 = thread2.result()
 
-        if len(labels2) < 2:
-            img2 = img1
-            img2_img = sitk.GetImageFromArray(img2)
-            sitk.WriteImage(img2_img, os.path.join(folder2, file2))
-    
-        for i, label in enumerate(labels2):
-            if img2_label_counts[i, 1] < threshold:
-                img2[img2 == label] = 0
-
-        labels = pd.unique(img1.flatten('K'))
-        
-        # parallelize
-        g1, cellcenter1 = compute_cell_location_fast(img1, labels)
-        g2, cellcenter2 = compute_cell_location_fast(img2, labels2)
-
-        if time == 0:
-            for cell in labels:
-                if cell != 0:
-                    string = '{} {} {} {}'.format(cell, time, time, 0)
-                    linelist.append(string)
-                maxtrackid = max(cell, maxtrackid)
-
+        # this line takes ~ 0.07s to calc
         maxtrackid, linelist = tracklet_fast(g1, g2, img1, img2, maxtrackid, time, linelist, folder1, cellcenter1, cellcenter2)
 
         print('--------%s seconds-----------' % (timing.time()-start_time))
@@ -226,4 +261,6 @@ def track_main_fast(seg_fold: str, track_fold: str):
         filetxt.write("\n")
     
     filetxt.close()
+
     print('whole time sequence running time %s' % (timing.time() - total_start_time))
+
